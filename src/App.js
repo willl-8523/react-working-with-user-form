f083611ae1427f643c5d22acfa6ab4c044004c70
willl85 <ngowilfried@gmail.com>
2023-08-31 14:53:37 +0200

    Remove SimpleInput Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import BasicForm from './components/BasicForm';
-import SimpleInput from './components/SimpleInput';
 
 function App() {
   return (
     <div className="app">
-      <SimpleInput />
       <BasicForm />
     </div>
   );
